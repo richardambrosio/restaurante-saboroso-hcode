f36bb798525ab0239a84750c03140e4cb83d42bb
Richard Ambrosio <richard.ambrosio1@live.com>
2021-11-23 11:44:25 -0300

    organizando o conteúdo com includes

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,16 @@
 var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
+var menus = require('./../inc/menus');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, results) => {
 
-    if (err) {
-      console.log(err);
-    }
-
+  menus.getMenus().then(results => {
     res.render('index', { 
       title: 'Restaurante Saboroso!',
       menus: results
     });
-
   });
 
 });
@@ -30,11 +24,16 @@ router.get('/contacts', function(req, res, next) {
 });
 
 router.get('/menus', function(req, res, next) {
-  res.render('menus', {
-    title: 'Menu - Restaurante Saboroso!',
-    background: 'images/img_bg_1.jpg',
-    h1: 'Saboreie nosso menu!'
+
+  menus.getMenus().then(results => {
+    res.render('menus', {
+      title: 'Menu - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!',
+      menus: results
+    });
   });
+  
 });
 
 router.get('/reservations', function(req, res, next) {
