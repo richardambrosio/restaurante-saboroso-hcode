9b9efbac5b04a24341028cbfeaa558b04f2a539c
Richard Ambrosio <richard.ambrosio1@live.com>
2021-11-24 15:58:59 -0300

    efetuando login na administração

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var users = require('./../inc/users')
 
 router.get("/", function(req, res, next) {
     res.render('admin/index');
 });
 
 router.get("/login", function(req, res, next) {
+    users.render(req, res, null)
+});
 
-    if (!req.session.views) req.session.views = 0;
+router.post('/login', function(req, res, next) {
+    if (!req.body.email) {
+        users.render(req, res, "Preencha o e-mail");
+    } else if (!req.body.password) {
+        users.render(req, res, "Preencha a senha");
+    } else {
 
-    console.log(req.session.views++);
+        users.login(req.body.email, req.body.password).then(user => {
 
-    res.render('admin/login');
+            req.session.user = user;
+
+            res.redirect('/admin');
+
+        }).catch(err => {
+            users.render(req, res, err.message || err);
+        });
+
+    }
 });
 
 router.get("/contacts", function(req, res, next) {
