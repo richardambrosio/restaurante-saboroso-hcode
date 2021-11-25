3ecb52aa885250fd18696f579dc43afb99bb181f
Richard Ambrosio <richard.ambrosio1@live.com>
2021-11-25 09:40:41 -0300

    Tela incial consultando dados no MySQL

--- inc/admin.js
+++ inc/admin.js
@@ -1,4 +1,24 @@
+const connection = require("./db");
+
 module.exports = {
+
+    dashboard() {
+        return new Promise((resolve, reject) => {
+            connection.query(`
+                SELECT
+                (SELECT COUNT(*) FROM tb_contacts) AS nrcontacts,
+                (SELECT COUNT(*) FROM tb_menus) AS nrmenus,
+                (SELECT COUNT(*) FROM tb_reservations) AS nrreservations,
+                (SELECT COUNT(*) FROM tb_users) AS nrusers;
+            `, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results[0]);
+                }
+            });
+        })
+    },
 
     getParams(req, params) {
         return Object.assign({}, {
