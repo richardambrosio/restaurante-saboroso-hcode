72e51d6df3976f49274051beeccae76e2751b873
Richard Ambrosio <richard.ambrosio1@live.com>
2021-11-25 10:50:52 -0300

    Administração do Menu - Consultando dados no MySQL

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var users = require('./../inc/users');
 var admin = require('./../inc/admin');
+var menus = require('./../inc/menus');
 
 router.use(function(req, res, next) {
 
@@ -73,7 +74,17 @@ router.get("/emails", function(req, res, next) {
 });
 
 router.get("/menus", function(req, res, next) {
-    res.render('admin/menus', admin.getParams(req));
+
+    menus.getMenus().then(data => {
+
+        res.render('admin/menus', admin.getParams(req, {
+            data
+        }));
+
+    }).catch(err => {
+        console.error(err);
+    });
+
 });
 
 router.get("/reservations", function(req, res, next) {
