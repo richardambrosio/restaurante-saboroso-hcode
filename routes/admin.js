6ecd1e12c9ff87d45721fef2ba6dabb2898d7de1
Richard Ambrosio <richard.ambrosio1@live.com>
2021-12-10 10:11:08 -0300

    Administração da reserva - create

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var users = require('./../inc/users');
 var admin = require('./../inc/admin');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 
 router.use(function(req, res, next) {
 
@@ -107,6 +108,22 @@ router.get("/reservations", function(req, res, next) {
     res.render('admin/reservations', admin.getParams(req, {
         date: {}
     }));
+});
+
+router.post("/reservations", function(req, res, next) {
+    reservations.save(req.fields, req.files).then(results => {
+        res.send(results);
+    }).catch(err => {
+        res.send(err);
+    });
+});
+
+router.delete("/reservations/:id", function(req, res, next) {
+    reservations.delete(req.params.id).then(results => {
+        res.send(results);
+    }).catch(err => {
+        res.send(err);
+    });
 });
 
 router.get("/users", function(req, res, next) {
