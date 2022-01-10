0810818a96ac02db125cb9696f46c68e38344368
Richard Ambrosio <richard.ambrosio1@live.com>
2022-01-10 12:44:01 -0300

    administração dos e-mails recebidos

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var menus = require('./../inc/menus');
 var reservations = require('./../inc/reservations');
 var contacts = require('./../inc/contacts');
+var emails = require('./../inc/emails');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -92,6 +93,16 @@ router.get('/services', function(req, res, next) {
     background: 'images/img_bg_1.jpg',
     h1: 'É um prazer poder servir!'
   });
+});
+
+router.post("/subscribe", function(req, res, next) {
+
+  emails.save(req).then(results => {
+    res.send(results);
+  }).catch(err => {
+    res.send(err);
+  });
+
 });
 
 module.exports = router;
