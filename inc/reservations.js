993a577220e73ee1f70c98fa9db3149b5ead1d07
Richard Ambrosio <richard.ambrosio1@live.com>
2022-01-11 10:26:57 -0300

    Obtendo dados do servidor e atualizando gráfico

--- inc/reservations.js
+++ inc/reservations.js
@@ -1,5 +1,6 @@
 var conn = require('./db');
 var Pagination = require('./../inc/Pagination');
+var moment = require('moment');
 
 module.exports = {
 
@@ -117,6 +118,45 @@ module.exports = {
             });
 
         });
+    },
+
+    chart(req) {
+        return new Promise((resolve, reject) => {
+
+            conn.query(`
+                SELECT
+                    CONCAT(YEAR(date), '-', MONTH(date)) AS date,
+                    COUNT(*) AS total,
+                    SUM(people) / COUNT(*) AS avg_people
+                FROM tb_reservations
+                WHERE
+                    date BETWEEN ? AND ?
+                GROUP BY date
+                ORDER BY date
+            `, [
+                req.query.start,
+                req.query.end
+            ], (err, results) => {
+
+                if (err) reject(err);
+                else {
+                    let months = [];
+                    let values = []
+
+                    results.forEach(row => {
+                        months.push(moment(row.date).format('MMM YYYY'));
+                        values.push(row.total);
+                    });
+
+                    resolve({
+                        months,
+                        values
+                    })
+                }
+
+            });
+
+        })
     }
 
 };
